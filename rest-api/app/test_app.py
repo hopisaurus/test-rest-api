398720e82c8b2cc684dc6cc972bca542da32cee7
Andrew <hopisaurus@gmail.com>
2022-10-25 23:28:18 -0400

    general fixes and cleanup

--- rest-api/app/test_app.py
+++ rest-api/app/test_app.py
@@ -1,9 +1,8 @@
-import pytest
-
 from app import app
 
 
 def test_request_example():
     response = app.test_client().get("/")
+    resp_test = '{"date": "1666735082","kubernetes": "false","version": "1.0.0"}'
     assert response.status_code == 200
-    # assert '{"date": "1666735082","kubernetes": "false","version": "1.0.0"}' in response.data
+    # assert resp_test in response.data

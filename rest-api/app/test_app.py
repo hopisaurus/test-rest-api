729fe8015c230144ef337a6728e431c7cdf16285
Andrew <hopisaurus@gmail.com>
2022-10-25 23:44:59 -0400

    add more tests

--- rest-api/app/test_app.py
+++ rest-api/app/test_app.py
@@ -6,3 +6,14 @@ def test_request_example():
     resp_test = '{"date": "1666735082","kubernetes": "false","version": "1.0.0"}'
     assert response.status_code == 200
     # assert resp_test in response.data
+
+
+def test_history():
+    response = app.test_client().get("/history")
+    assert response.status_code == 200
+
+
+def test_lookup_validate():
+    # test post call /v1/lookup/validate
+    response = app.test_client().post("/v1/lookup/validate", json={"ip": "0.0.0.0"})
+    assert response.status_code == 200

be501f1c26d42184c70ce000af9e76b2e82bdc0f
Andrew <hopisaurus@gmail.com>
2022-10-26 00:12:54 -0400

    reformatting

--- rest-api/tests/test_app.py
+++ rest-api/tests/test_app.py
@@ -3,7 +3,7 @@ from app import app
 
 def test_request_example():
     response = app.test_client().get("/")
-    resp_test = '{"date": "1666735082","kubernetes": "false","version": "1.0.0"}'
+    # resp_test = '{"date": "1666735082","kubernetes": "false","version": "1.0.0"}'
     assert response.status_code == 200
     # assert resp_test in response.data
 
